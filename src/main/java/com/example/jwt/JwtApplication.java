dc80608310484784fb86723e09c880b521e618a6
ud17 <upandya1297@gmail.com>
2023-07-01 17:55:18 -0400

    init commit

---
+++ src/main/java/com/example/jwt/JwtApplication.java
@@ -0,0 +1,18 @@
+package com.example.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"com.example.controllers"})
+@EntityScan(basePackages = {"com.example.models"})
+public class JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/models/User.java
@@ -0,0 +1,62 @@
+package com.example.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+// maps the User class with table in the database
+@Entity
+@Table(name = "Users", schema = "public")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String firstname;
+    private String lastname;
+
+    private String email;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
