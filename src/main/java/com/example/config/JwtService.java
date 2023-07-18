23d33f6077a398b9d5ef55c904f2cdf000cd4bc2
ud17 <upandya1297@gmail.com>
2023-07-18 17:06:27 -0400

    jwt auth service

---
+++ src/main/java/com/example/config/JwtService.java
@@ -0,0 +1,10 @@
+package com.example.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String jwtToken) {
+        return "";
+    }
+}
---
+++ src/main/java/com/example/models/Role.java
@@ -0,0 +1,6 @@
+package com.example.models;
+
+public enum Role {
+    USER,
+    ADMIN
+}
